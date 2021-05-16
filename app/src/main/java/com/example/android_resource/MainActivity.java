1c61ccf9837a358b27cadf43b99a20c489621bfc
Francisco Canto <2160781@my.ipleiria.pt>
2021-05-16 10:36:37 +0100

    Bug do ListView resolvido, a app funciona sem problemas

--- app/src/main/java/com/example/android_resource/MainActivity.java
+++ app/src/main/java/com/example/android_resource/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.android_resource;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +45,37 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(anterior11);
                 break;
             case R.id.mTarefas:
-                Intent anterior21 = new Intent(this, ListView.class);
-                startActivity(anterior21);
-                return true;
-        }
 
-        return super.onOptionsItemSelected(item);
+                setContentView(R.layout.activity_listview);
+
+                listView = findViewById(R.id.listview);
+
+                String[] values = new String[] {
+                        "Facebook", "Instagram", "Twitter", "Youtube",
+                        "Tumbler", "WhatsApp", "Wechat", "Telegram",
+                        "Twitch", "Github", "Snapchat", "Viber",
+                        "Android Rion"};
+
+                        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                                android.R.layout.simple_list_item_1,
+                                android.R.id.text1, values);
+
+                listView.setAdapter(adapter);
+
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        if(position == 0){
+                            Intent intent = new Intent(view.getContext(),
+                                    Activity_facebook.class);
+                            startActivity(intent);
+                        }
+                    }
+                });
+
+            }
+                return true;
+
     }
 }
